046a60f99350181d28e9cfea30586a5f43b8d948
Jacob <pumear0@gmail.com>
2018-09-21 15:21:39 -0400

    changed design a bit, added xml parsing for getting config options, made config option file
    changed design a bit, added xml parsing for getting config options, made config option file

--- jhuras_mmaxner_SOA_a2/MainForm.Designer.cs
+++ jhuras_mmaxner_SOA_a2/MainForm.Designer.cs
@@ -28,117 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.cmbService = new System.Windows.Forms.ComboBox();
-			this.cmbMethod = new System.Windows.Forms.ComboBox();
-			this.lblService = new System.Windows.Forms.Label();
-			this.lblMethod = new System.Windows.Forms.Label();
-			this.lblResponse = new System.Windows.Forms.Label();
-			this.gridResponse = new System.Windows.Forms.DataGridView();
-			this.btnInvoke = new System.Windows.Forms.Button();
-			this.lblParameters = new System.Windows.Forms.Label();
-			this.txtArguments = new System.Windows.Forms.TextBox();
-			((System.ComponentModel.ISupportInitialize)(this.gridResponse)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// cmbService
-			// 
-			this.cmbService.FormattingEnabled = true;
-			this.cmbService.Location = new System.Drawing.Point(75, 12);
-			this.cmbService.Name = "cmbService";
-			this.cmbService.Size = new System.Drawing.Size(121, 21);
-			this.cmbService.TabIndex = 0;
-			// 
-			// cmbMethod
-			// 
-			this.cmbMethod.FormattingEnabled = true;
-			this.cmbMethod.Location = new System.Drawing.Point(75, 39);
-			this.cmbMethod.Name = "cmbMethod";
-			this.cmbMethod.Size = new System.Drawing.Size(121, 21);
-			this.cmbMethod.TabIndex = 1;
-			// 
-			// lblService
-			// 
-			this.lblService.AutoSize = true;
-			this.lblService.Location = new System.Drawing.Point(26, 15);
-			this.lblService.Name = "lblService";
-			this.lblService.Size = new System.Drawing.Size(43, 13);
-			this.lblService.TabIndex = 2;
-			this.lblService.Text = "Service";
-			// 
-			// lblMethod
-			// 
-			this.lblMethod.AutoSize = true;
-			this.lblMethod.Location = new System.Drawing.Point(26, 42);
-			this.lblMethod.Name = "lblMethod";
-			this.lblMethod.Size = new System.Drawing.Size(43, 13);
-			this.lblMethod.TabIndex = 3;
-			this.lblMethod.Text = "Method";
-			// 
-			// lblResponse
-			// 
-			this.lblResponse.AutoSize = true;
-			this.lblResponse.Location = new System.Drawing.Point(14, 93);
-			this.lblResponse.Name = "lblResponse";
-			this.lblResponse.Size = new System.Drawing.Size(55, 13);
-			this.lblResponse.TabIndex = 4;
-			this.lblResponse.Text = "Response";
-			// 
-			// gridResponse
-			// 
-			this.gridResponse.AllowUserToAddRows = false;
-			this.gridResponse.AllowUserToDeleteRows = false;
-			this.gridResponse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.gridResponse.Location = new System.Drawing.Point(75, 93);
-			this.gridResponse.Name = "gridResponse";
-			this.gridResponse.ReadOnly = true;
-			this.gridResponse.Size = new System.Drawing.Size(397, 235);
-			this.gridResponse.TabIndex = 5;
-			// 
-			// btnInvoke
-			// 
-			this.btnInvoke.Location = new System.Drawing.Point(397, 64);
-			this.btnInvoke.Name = "btnInvoke";
-			this.btnInvoke.Size = new System.Drawing.Size(75, 23);
-			this.btnInvoke.TabIndex = 6;
-			this.btnInvoke.Text = "Invoke";
-			this.btnInvoke.UseVisualStyleBackColor = true;
-			this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_ClickAsync);
-			// 
-			// lblParameters
-			// 
-			this.lblParameters.AutoSize = true;
-			this.lblParameters.Location = new System.Drawing.Point(12, 69);
-			this.lblParameters.Name = "lblParameters";
-			this.lblParameters.Size = new System.Drawing.Size(57, 13);
-			this.lblParameters.TabIndex = 7;
-			this.lblParameters.Text = "Arguments";
-			// 
-			// txtArguments
-			// 
-			this.txtArguments.Location = new System.Drawing.Point(75, 66);
-			this.txtArguments.Name = "txtArguments";
-			this.txtArguments.Size = new System.Drawing.Size(316, 20);
-			this.txtArguments.TabIndex = 8;
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 350);
-			this.Controls.Add(this.txtArguments);
-			this.Controls.Add(this.lblParameters);
-			this.Controls.Add(this.btnInvoke);
-			this.Controls.Add(this.gridResponse);
-			this.Controls.Add(this.lblResponse);
-			this.Controls.Add(this.lblMethod);
-			this.Controls.Add(this.lblService);
-			this.Controls.Add(this.cmbMethod);
-			this.Controls.Add(this.cmbService);
-			this.Name = "MainForm";
-			this.Text = "Assignment 2 - Web Services";
-			((System.ComponentModel.ISupportInitialize)(this.gridResponse)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.cmbService = new System.Windows.Forms.ComboBox();
+            this.cmbMethod = new System.Windows.Forms.ComboBox();
+            this.lblService = new System.Windows.Forms.Label();
+            this.lblMethod = new System.Windows.Forms.Label();
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.gridResponse = new System.Windows.Forms.DataGridView();
+            this.btnInvoke = new System.Windows.Forms.Button();
+            this.lblParameters = new System.Windows.Forms.Label();
+            this.gridArguments = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridResponse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridArguments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbService
+            // 
+            this.cmbService.FormattingEnabled = true;
+            this.cmbService.Location = new System.Drawing.Point(75, 12);
+            this.cmbService.Name = "cmbService";
+            this.cmbService.Size = new System.Drawing.Size(121, 21);
+            this.cmbService.TabIndex = 0;
+            // 
+            // cmbMethod
+            // 
+            this.cmbMethod.FormattingEnabled = true;
+            this.cmbMethod.Location = new System.Drawing.Point(75, 39);
+            this.cmbMethod.Name = "cmbMethod";
+            this.cmbMethod.Size = new System.Drawing.Size(121, 21);
+            this.cmbMethod.TabIndex = 1;
+            // 
+            // lblService
+            // 
+            this.lblService.AutoSize = true;
+            this.lblService.Location = new System.Drawing.Point(26, 15);
+            this.lblService.Name = "lblService";
+            this.lblService.Size = new System.Drawing.Size(43, 13);
+            this.lblService.TabIndex = 2;
+            this.lblService.Text = "Service";
+            // 
+            // lblMethod
+            // 
+            this.lblMethod.AutoSize = true;
+            this.lblMethod.Location = new System.Drawing.Point(26, 42);
+            this.lblMethod.Name = "lblMethod";
+            this.lblMethod.Size = new System.Drawing.Size(43, 13);
+            this.lblMethod.TabIndex = 3;
+            this.lblMethod.Text = "Method";
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.AutoSize = true;
+            this.lblResponse.Location = new System.Drawing.Point(14, 254);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(55, 13);
+            this.lblResponse.TabIndex = 4;
+            this.lblResponse.Text = "Response";
+            // 
+            // gridResponse
+            // 
+            this.gridResponse.AllowUserToAddRows = false;
+            this.gridResponse.AllowUserToDeleteRows = false;
+            this.gridResponse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridResponse.Location = new System.Drawing.Point(75, 254);
+            this.gridResponse.Name = "gridResponse";
+            this.gridResponse.ReadOnly = true;
+            this.gridResponse.Size = new System.Drawing.Size(397, 150);
+            this.gridResponse.TabIndex = 4;
+            // 
+            // btnInvoke
+            // 
+            this.btnInvoke.Location = new System.Drawing.Point(75, 225);
+            this.btnInvoke.Name = "btnInvoke";
+            this.btnInvoke.Size = new System.Drawing.Size(75, 23);
+            this.btnInvoke.TabIndex = 3;
+            this.btnInvoke.Text = "Invoke";
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_ClickAsync);
+            // 
+            // lblParameters
+            // 
+            this.lblParameters.AutoSize = true;
+            this.lblParameters.Location = new System.Drawing.Point(12, 69);
+            this.lblParameters.Name = "lblParameters";
+            this.lblParameters.Size = new System.Drawing.Size(57, 13);
+            this.lblParameters.TabIndex = 7;
+            this.lblParameters.Text = "Arguments";
+            // 
+            // gridArguments
+            // 
+            this.gridArguments.AllowUserToAddRows = false;
+            this.gridArguments.AllowUserToDeleteRows = false;
+            this.gridArguments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridArguments.Location = new System.Drawing.Point(75, 69);
+            this.gridArguments.Name = "gridArguments";
+            this.gridArguments.Size = new System.Drawing.Size(397, 150);
+            this.gridArguments.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 414);
+            this.Controls.Add(this.gridArguments);
+            this.Controls.Add(this.lblParameters);
+            this.Controls.Add(this.btnInvoke);
+            this.Controls.Add(this.gridResponse);
+            this.Controls.Add(this.lblResponse);
+            this.Controls.Add(this.lblMethod);
+            this.Controls.Add(this.lblService);
+            this.Controls.Add(this.cmbMethod);
+            this.Controls.Add(this.cmbService);
+            this.Name = "MainForm";
+            this.Text = "Assignment 2 - Web Services";
+            ((System.ComponentModel.ISupportInitialize)(this.gridResponse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridArguments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -152,7 +157,7 @@
         private System.Windows.Forms.DataGridView gridResponse;
 		private System.Windows.Forms.Button btnInvoke;
 		private System.Windows.Forms.Label lblParameters;
-		private System.Windows.Forms.TextBox txtArguments;
-	}
+        private System.Windows.Forms.DataGridView gridArguments;
+    }
 }
 
--- jhuras_mmaxner_SOA_a2/MainForm.cs
+++ jhuras_mmaxner_SOA_a2/MainForm.cs
@@ -9,32 +9,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace SOA___Assignment_2___Web_Services
 {
-    public partial class MainForm : System.Windows.Forms.Form
-    {
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+        const string _CONFIG_FILENAME = "config.xml";
+        XDocument _soapConfig;
+
+
         public MainForm()
-        {
-            InitializeComponent();
-			// populate combo box 1 with whatever is in the txt file
-			populateServices();
+		{
+			InitializeComponent();
+
+            // load all settings in the xml config file
+            loadSoapConfig();
+
+            // populate combo box 1 with whatever is in the txt file
+            populateServices();
 
 			// populate combo box 2 with a list of methods from the first url in the txt file (selected in combo box 1)
-			populateActions("replace me");
+			populateActions(cmbService.SelectedText);
 		}
 
 		private async void btnInvoke_ClickAsync(object sender, EventArgs e)
 		{
-			//string url = (cmbService.SelectedItem as ComboBoxItem).Value;
-			//string action = (cmbMethod.SelectedItem as ComboBoxItem).Value;
-			//List<string> parameters = txtArguments.Text.Split(',').OfType<string>().ToList(); // convert parameters to separated list
+            string url = (cmbService.SelectedItem as ComboBoxItem).Value;
+            string action = (cmbMethod.SelectedItem as ComboBoxItem).Value;
+            List<string> parameters = txtArguments.Text.Split(',').OfType<string>().ToList(); // convert parameters to separated list
 
-			// todo - remove magic numbers, very bad
-			string url = "http://www.dneonline.com/calculator.asmx";
-			string action = "Add";
-
-			string result = WebServiceFramework.CallWebService(url, action);
+            string result = WebServiceFramework.CallWebService(url, action);
 			if (!string.IsNullOrEmpty(result))
 			{
 				using (Stream resultStream = GenerateStreamFromString(result))
@@ -44,18 +49,50 @@ namespace SOA___Assignment_2___Web_Services
 			}
 		}
 
+        private void loadSoapConfig()
+        {
+            try
+            {
+                _soapConfig = XDocument.Load(_CONFIG_FILENAME);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK);
+            }
+        }
+
 		private void populateServices()
 		{
-			// todo
-		}
+            List<string> services = _soapConfig.Descendants("services").Elements("service").Elements("name").Select(e => e.Value).ToList();
+            List<string> urls = _soapConfig.Descendants("services").Elements("service").Elements("url").Select(e => e.Value).ToList();
+            for (int i = 0; i < urls.Count; i++)
+            {
+                cmbService.Items.Add(new ComboBoxItem() { Text = services[i], Value = urls[i] });
+            }
+
+            if (cmbService.Items.Count > 0)
+            {
+                cmbService.SelectedIndex = 0;
+            }
+        }
 
 		private void populateActions(string serviceName)
 		{
-			// todo
-		}
+            List<string> actions = _soapConfig.Descendants("services").Elements("service").Elements("action").Elements("name").Select(e => e.Value).ToList();
+            foreach (var action in actions)
+            {
+                cmbMethod.Items.Add(new ComboBoxItem() { Text = action, Value = action });
+            }
 
-		//https://stackoverflow.com/questions/1879395/how-do-i-generate-a-stream-from-a-string
-		private Stream GenerateStreamFromString(string s)
+            if (cmbMethod.Items.Count > 0)
+            {
+                cmbMethod.SelectedIndex = 0;
+            }
+        }
+
+        //https://stackoverflow.com/questions/1879395/how-do-i-generate-a-stream-from-a-string
+        private Stream GenerateStreamFromString(string s)
 		{
 			var stream = new MemoryStream();
 			var writer = new StreamWriter(stream);
